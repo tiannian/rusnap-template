671e997dda88ffd9311e195caf1a21d846d050a3
tiannian <dtiannian@gmail.com>
2023-10-13 01:11:49 +0800

    update main

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,11 @@
-use rusnap::{handler, Route};
+use rusnap::{handler, main, Route};
 
 #[handler]
 pub async fn handle_hello() -> String {
     "hello world".into()
 }
 
+#[main]
 async fn main() {
     Route::new(()).at("hello", handle_hello).serve();
 }
-
-rusnap::entry!(main);
